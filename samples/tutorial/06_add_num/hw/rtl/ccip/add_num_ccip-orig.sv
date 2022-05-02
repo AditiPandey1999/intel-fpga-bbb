6cb6e73173d36f9d65aac1888e39dd6b00fc94fe
AditiPandey1999 <104061647+AditiPandey1999@users.noreply.github.com>
2022-05-02 12:57:08 +0530

    changed sources.txt

--- samples/tutorial/06_add_num/hw/rtl/ccip/add_num_ccip-orig.sv
+++ samples/tutorial/06_add_num/hw/rtl/ccip/add_num_ccip-orig.sv
@@ -199,7 +199,7 @@ module ofs_plat_afu
     begin
         if (is_mem_addr_csr_write)
         begin
-            mem_addr <= t_ccip_clAddr'(host_ccip.sRx.c0.data[63:0]);
+            mem_addr <= t_ccip_clAddr'(host_ccip.sRx.c0.data);
         end
     end
     
@@ -270,13 +270,10 @@ module ofs_plat_afu
         // Start of packet is always set for single beat writes
         wr_hdr.sop = 1'b1;
     end
-    integer a= host_ccip.sRx.c0.data[64];
-    integer b= host_ccip.sRx.c0.data[71];
-    integer res= a+b;
 
     // Data to write to memory: little-endian hardcoded ASCII encoding of sum
-    //assign host_ccip.sTx.c1.data = t_ccip_clData'('h23);
-    assign host_ccip.sTx.c1.data = res;
+   assign host_ccip.sTx.c1.data = t_ccip_clData'('h23);
+
     // Control logic for memory writes
     always_ff @(posedge clk)
     begin
