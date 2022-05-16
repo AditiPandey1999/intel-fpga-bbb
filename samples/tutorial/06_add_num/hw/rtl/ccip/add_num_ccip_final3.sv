0bfcc8387685dee9efe06196873e00faf543d35a
AditiPandey1999 <104061647+AditiPandey1999@users.noreply.github.com>
2022-05-16 15:11:18 +0530

    Update add_num_ccip_final3.sv

--- samples/tutorial/06_add_num/hw/rtl/ccip/add_num_ccip_final3.sv
+++ samples/tutorial/06_add_num/hw/rtl/ccip/add_num_ccip_final3.sv
@@ -331,9 +331,7 @@ module ofs_plat_afu
             begin
                 // Control logic for memory writes
                 // Request the write as long as the channel isn't full.
-                a = mem_read_data[15:8];
-                b = mem_read_data[23:16];
-                res = a+b;
+                res <= a+b;
                 host_ccip.sTx.c1.hdr <= wr_hdr;
                 host_ccip.sTx.c1.data <= t_ccip_clData'(res);
                 host_ccip.sTx.c1.valid <= (! host_ccip.sRx.c1TxAlmFull);
