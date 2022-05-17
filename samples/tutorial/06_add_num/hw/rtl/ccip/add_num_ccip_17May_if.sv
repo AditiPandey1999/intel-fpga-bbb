8fb593994191eaa6fd64f33522c53fd8c702531b
AditiPandey1999 <104061647+AditiPandey1999@users.noreply.github.com>
2022-05-17 17:13:52 +0530

    Update add_num_ccip_17May_if.sv

--- samples/tutorial/06_add_num/hw/rtl/ccip/add_num_ccip_17May_if.sv
+++ samples/tutorial/06_add_num/hw/rtl/ccip/add_num_ccip_17May_if.sv
@@ -213,7 +213,8 @@ module ofs_plat_afu
         // Let the FIU pick the channel
         //rd_hdr.vc_sel = t_ccip_vc(2'h0);
 
-        rd_hdr.cl_len = 2'h0;
+        //rd_hdr.cl_len = 2'h0;
+        cast(rd_hdr.cl_len,0);
     end
 
     
