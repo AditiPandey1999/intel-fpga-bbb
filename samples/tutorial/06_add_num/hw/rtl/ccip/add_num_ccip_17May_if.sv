3f880f68f3c8685695288038afe310d6b8d2dcef
AditiPandey1999 <104061647+AditiPandey1999@users.noreply.github.com>
2022-05-18 10:34:08 +0530

    Update add_num_ccip_17May_if.sv

--- samples/tutorial/06_add_num/hw/rtl/ccip/add_num_ccip_17May_if.sv
+++ samples/tutorial/06_add_num/hw/rtl/ccip/add_num_ccip_17May_if.sv
@@ -319,7 +319,7 @@ module ofs_plat_afu
                 else
                 begin
                     //rsp_hdr <= t_ccip_c0_RspMemHdr'(0);
-                    display("Else Read response");
+                    $display("Else Read response");
                     host_ccip.sTx.c1.data <= t_ccip_clData'(40);
                     state <= STATE_NUM;
                 end
